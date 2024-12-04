c91393bc5aa7df28832011717e96efa856aadb32
DerionbA <alemdavis8@gmail.com>
2024-12-04 17:05:10 -0400

    all in one

---
+++ my List.py
@@ -0,0 +1,52 @@
+mylist=['apple', 'graph', 'banana', 'dragon fruit', 'guava']
+print("Length of list:", len(mylist))
+print("First Element:", mylist[0])
+print("Last Element:", mylist[-1])
+
+
+print("Last Element:", mylist[-1])
+
+
+mylist.append('Papaya')
+print("Updated List:", mylist)
+
+
+
+
+mylist.remove('Guava')
+print("Updated List:", mylist)
+
+
+mylist.sort()
+
+
+print("Sorted List:", mylist)
+
+
+mylist.pop(1)
+
+
+print("Updated List :", mylist)
+
+
+mylist.reverse()
+
+
+print("Reversed List", mylist)
+
+
+print("Multiplication on list", mylist * 2)
+
+
+mylist = lst[:4]
+
+
+print("Sliced List :", mylist)
+
+
+mylist.clear()
+
+
+print("Updated myList", mylist)
+
+
